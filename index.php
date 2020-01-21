5b8a8d13fa259b832e3cc9d5aa2e73af0ff9c87f
Matthias <45299216+MaScheller@users.noreply.github.com>
2020-01-21 13:56:12 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<title>Spiele Auswahl</title>
+    <link rel="stylesheet" href="CSS/normalize.min.css">
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    <div class="background">
+        <h1 class="bold-heading">Spiele Auswahl</h1>     
+        <div class="desktop-12 mobile-12 row">
+            <a href="Games/2048" class="button desktop-5 float-left mobile-12">2048</a>
+        
+            <a href="Games/FlappyBirds" class="button desktop-5 float-right mobile-12">Flappy Birds</a>
+        </div>     
+        <div class="desktop-12 mobile-12 row">
+            <a href="Games/Snake" class="button desktop-5 float-left mobile-12">Snake</a>
+       </div>
+       <b class="bottom">Created by Matthias Scheller <br>
+	   <b>Den Quellcode finden Sie auf <a href="https://github.com/MaScheller/Games" target="_blank">Github</a></b>
+      </div>
+      
+          
+</body>    
+</html>
